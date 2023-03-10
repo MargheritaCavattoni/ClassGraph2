987a3b2120936956585a7f6c665710580f45efa7
MargheritaCavattoni <cavattonimargherita@gmail.com>
2023-03-10 17:34:34 +0100

    Add preprocessing

---
+++ Preprocessing/CMakeFiles/paftk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paftk.dir/main.c.o"
+  "paftk"
+  "paftk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/paftk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
